c1b89d6d21bc9a6455bfa1d57b22a42d104d820a
Fabio C <fabio.c.colaco@gmail.com>
2020-05-05 17:27:41 +0200

    Bootstrap + create Model Aventure + create AventureController + create View Aventure

--- jeudontvousetesleheros.Web.UI/Controllers/AventureController.cs
+++ jeudontvousetesleheros.Web.UI/Controllers/AventureController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using jeudontvousetesleheros.Web.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace jeudontvousetesleheros.Web.UI.Controllers
@@ -10,7 +11,23 @@ namespace jeudontvousetesleheros.Web.UI.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            this.ViewBag.MonTitre = "Aventures";
+
+            List<Aventure> maList = new List<Aventure>();
+
+            maList.Add(new Aventure()
+            {
+                Id = 1,
+                Titre = "ma premiere aventure"
+            }); 
+            
+            maList.Add(new Aventure()
+            {
+                Id = 2,
+                Titre = "ma seconde aventure"
+            });
+
+            return View(maList);
         }
     }
 }
---
+++ jeudontvousetesleheros.Web.UI/Models/Aventure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontvousetesleheros.Web.UI.Models
+{
+    public class Aventure
+    {
+        #region Propriétés
+
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        #endregion
+    }
+}
