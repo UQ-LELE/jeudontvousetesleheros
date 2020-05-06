f2cddee7658568354b320beb29e37ab0640ffd83
Fabio C <fabio.c.colaco@gmail.com>
2020-05-06 17:16:46 +0200

    Project Web.UI => add URL Friendly for Aventure/Index

--- jeudontvousetesleheros.Web.UI/Startup.cs
+++ jeudontvousetesleheros.Web.UI/Startup.cs
@@ -54,6 +54,15 @@ namespace jeudontvousetesleheros.Web.UI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "mesaventures",
+                  pattern: "mes-aventures",
+                  defaults: new
+                  {
+                      controller = "Aventure",
+                      action = "Index"
+                  });
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
