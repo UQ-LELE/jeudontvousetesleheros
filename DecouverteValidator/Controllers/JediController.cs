f39c4736530b6e732745e8bed7bb6b13a7e0c658
Fabio C <fabio.c.colaco@gmail.com>
2020-05-07 17:18:20 +0200

    POC => DecouverteValidator

---
+++ DecouverteValidator/Controllers/JediController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DecouverteValidator.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DecouverteValidator.Controllers
+{
+    public class JediController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Jedi jedi)
+        {
+            if (ModelState.IsValid)
+            {
+                //Je pourrai allez enregistrer en bdd
+            }
+
+            return this.View(jedi);
+        }
+    }
+}
